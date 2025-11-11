460f482efc46ec7f1b39f8ba262d05aa970e3010
caden <ckp634@my.utexas.edu>
2025-11-11 11:00:48 -0600

    more strucutrre

--- teleop.py
+++ teleop.py
@@ -2,6 +2,48 @@ import mediapipe
 import cv2
 import mujoco
 import multiprocessing
+import gymnasium as gym
+import pygame
+import stable-baselines3 as sb3
 
 
-class landmark
+class gym:
+    def __init__(self):
+        pass
+    def create_env(self):
+        pass
+    def reset_env(self):
+        pass
+    def step_env(self):
+        pass
+
+class control:
+    def __init__(self):
+        pass
+    def keyboard(self):
+        pass
+    def handpose(self):
+        pass
+    def logging(self):
+        pass
+
+class rendering:
+    def __init__(self):
+        pass
+    def live_display(self):
+        pass
+    def trained_display(self):
+        pass
+
+class learning:
+    def __init__(self):
+        pass
+    def train_model(self):
+        pass
+    def load_model(self):
+        pass
+    def save_model(self):
+        pass
+    def evaluate_model(self):
+        pass
+
